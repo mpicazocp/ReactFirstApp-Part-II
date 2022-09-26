644f46f40f1a44173cd29d81b6b7f104b369fa1c
mpicazocp <mpicazo@calpoly.edu>
2022-09-26 13:31:46 -0700

    find by ID

--- expressjs-backend/backend.js
+++ expressjs-backend/backend.js
@@ -21,6 +21,24 @@ app.get('/users', (req, res) => {
     }
 });
 
+app.get('/users/:id', (req, res) => {
+    const id = req.params['id']; //or req.params.id
+    let result = findUserById(id);
+    if (result === undefined || result.length == 0)
+        res.status(404).send('Resource not found.');
+    else {
+        result = {users_list: result};
+        res.send(result);
+    }
+});
+
+//HELPER FUNCTIONS
+
+function findUserById(id) {
+    return users['users_list'].find( (user) => user['id'] === id); // or line below
+    //return users['users_list'].filter( (user) => user['id'] === id);
+}
+
 const findUserByName = (name) => { 
     return users['users_list'].filter( (user) => user['name'] === name); 
 }
@@ -30,7 +48,7 @@ app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 }); 
 
-
+//USERs struct
 const users = { 
     users_list :
     [
