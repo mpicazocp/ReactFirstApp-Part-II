e2d5997a7256d1b329ceecef1b8dec1e8b9da2ff
mpicazocp <mpicazo@calpoly.edu>
2022-09-26 13:26:25 -0700

    checkin for macs

--- expressjs-backend/backend.js
+++ expressjs-backend/backend.js
@@ -10,8 +10,20 @@ app.get('/', (req, res) => {
 });
 
 app.get('/users', (req, res) => {
-    res.send(users);
+    const name = req.query.name;
+    if (name != undefined){
+        let result = findUserByName(name);
+        result = {users_list: result};
+        res.send(result);
+    }
+    else{
+        res.send(users);
+    }
 });
+
+const findUserByName = (name) => { 
+    return users['users_list'].filter( (user) => user['name'] === name); 
+}
 
 //LISTEN REQUESTS
 app.listen(port, () => {
