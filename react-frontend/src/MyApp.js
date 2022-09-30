16345de84605bc1c36a411bf08550a384d2d974e
mpicazocp <mpicazo@calpoly.edu>
2022-09-30 13:32:40 -0700

    step 5 done

--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -1,5 +1,5 @@
 import Form from './Form';
-import React, {useState} from 'react';
+//import React, {useState} from 'react';
 import Table from './Table'
 import axios from 'axios';
 import React, {useState, useEffect} from 'react';
@@ -15,9 +15,12 @@ function MyApp() {
         });
         setCharacters(updated);
   }
-  function updateList(person) {
-      setCharacters([...characters, person]);
-  }
+  function updateList(person) { 
+    makePostCall(person).then( result => {
+    if (result && result.status === 200)
+       setCharacters([...characters, person] );
+    });
+ }
   async function fetchAll(){
     try {
         const response = await axios.get('http://localhost:5000/users');
@@ -29,6 +32,18 @@ function MyApp() {
         return false;         
     }
   }
+
+  async function makePostCall(person){
+    try {
+       const response = await axios.post('http://localhost:5000/users', person);
+       return response;
+    }
+    catch (error) {
+       console.log(error);
+       return false;
+    }
+ }
+
   useEffect(() => {
     fetchAll().then( result => {
        if (result)
